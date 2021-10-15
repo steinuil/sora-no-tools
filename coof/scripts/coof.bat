73a1ccf343ed07d57830fc4db5c9a64cf2805b28
steinuil <steenuil.owl@gmail.com>
2021-10-15 18:51:14 +0200

    moved coof scripts to coof/scripts

--- coof/scripts/coof.bat
+++ coof/scripts/coof.bat
@@ -1,3 +1,3 @@
 start "mpv" mpv --input-ipc-server=\\.\pipe\tmp\mpv-socket --scripts=.\coof.js --video-sync=display-resample --pause=no https://stream.wotos.eu/snw_master.m3u8
 timeout /T 2
-coof http://stream.wotos.eu/spreader \\.\pipe\tmp\mpv-socket
+coof --server http://stream.wotos.eu/spreader --mpv-socket \\.\pipe\tmp\mpv-socket
--- coof/scripts/coof.sh
+++ coof/scripts/coof.sh
@@ -1,3 +1,3 @@
 mpv --input-ipc-server=/tmp/mpv-socket --scripts=./coof.js --video-sync=display-resample --pause=no https://stream.wotos.eu/snw_master.m3u8 &
 sleep 2
-./coof http://stream.wotos.eu/spreader /tmp/mpv-socket
+./coof --server http://stream.wotos.eu/spreader --mpv-socket /tmp/mpv-socket
